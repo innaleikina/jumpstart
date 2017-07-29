6883dd0d008f1b9e2dc8c6f1d2b86d76dffaade3
Inna <designbyinna@gmail.com>
2017-07-29 16:12:02 -0600

    slick set up

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
   $('.carousel').slick({
-    slidesToShow: 1,
-  
+    //slidesToShow: 1,
+    //autoplay: true,
+    //autoplaySpeed: 4000,
+  //arrows:false,
+  //  draggable: false,
+  //  infinite: true,
+  //  pauseOnHover: false,
+  //  pauseOnFocus: false,
+  //  adaptiveHeight: true
   });
+
+});
